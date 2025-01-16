d319b761bfb6d921cedece2ab15c42ca679a6b9b
Mark Carson <markcarson121@gmail.com>
2025-01-16 15:47:11 -0300

    removed reference to custom map

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,14 +1,5 @@
 // import { Company } from "./Company";
 // import { User } from "./User";
+import { CustomMap } from "./CustomMap";
 
-const mapElement = document.getElementById("map");
-
-if (mapElement) {
-  new google.maps.Map(mapElement, {
-    zoom: 1,
-    center: {
-      lat: 0,
-      lng: 0,
-    },
-  });
-}
+new CustomMap();
