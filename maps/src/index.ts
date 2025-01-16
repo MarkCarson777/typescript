a6b37469976a90aeb253273abc5b8357cff843ff
Mark Carson <markcarson121@gmail.com>
2025-01-16 15:22:48 -0300

    rendered map

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,8 +1,10 @@
-import { Company } from "./Company";
-import { User } from "./User";
+// import { Company } from "./Company";
+// import { User } from "./User";
 
-const user = new User();
-const company = new Company();
-
-console.log("user", user);
-console.log("company", company);
+new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
